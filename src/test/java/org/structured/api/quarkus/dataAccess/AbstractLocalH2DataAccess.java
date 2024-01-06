ac54096ceccec52c6c1ae56ad5354760a17a731a
Alexandru Agache <agache41@yahoo.com>
2024-01-06 15:07:42 +0100

    fixing tests 2

--- src/test/java/org/structured/api/quarkus/dataAccess/AbstractLocalH2DataAccess.java
+++ src/test/java/org/structured/api/quarkus/dataAccess/AbstractLocalH2DataAccess.java
@@ -32,6 +32,7 @@ public class AbstractLocalH2DataAccess<ENTITY extends PrimaryKey<PK>, PK> extend
     public void beginTransaction() {
         EntityTransaction transaction = this.em.getTransaction();
         transaction.begin();
+        System.out.println("Transaction Begin");
     }
 
     public void commitTransaction() {
@@ -39,5 +40,6 @@ public class AbstractLocalH2DataAccess<ENTITY extends PrimaryKey<PK>, PK> extend
         EntityTransaction transaction = this.em.getTransaction();
         transaction.commit();
         this.em.clear();
+        System.out.println("Transaction Commit");
     }
 }
